8d674cec3699576514409b345cb27735029291b0
admiravelhomemdasneves <andre.santos.loureiro@gmail.com>
2025-04-16 17:59:13 +0100

    Fix http protocol

--- src/api/apiClient.jsx
+++ src/api/apiClient.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const BASE_URL = 'http://localhost:8080';
-const BASE_URL = 'http://cat-app-g9audugfc0fmdpax.westeurope-01.azurewebsites.net';
+const BASE_URL = 'cat-app-g9audugfc0fmdpax.westeurope-01.azurewebsites.net';
 
 class ApiClient {
   constructor() {
